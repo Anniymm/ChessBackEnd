168175de72299441333268d7522ce3af9565089e
Anniymm <animaisuradze1999@gmail.com>
2024-09-22 17:02:02 +0400

    add settings for sending invitations

--- project/settings.py
+++ project/settings.py
@@ -130,3 +130,11 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'chssclock@gmail.com'  
+EMAIL_HOST_PASSWORD = 'vbxplqttljwwuqhd'  #satesto gmail, shecvla nebismier dros sheidzleba aqedanve
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
