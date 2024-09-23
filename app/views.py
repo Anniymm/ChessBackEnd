84c4cccff684cd7d8e69017774c0f5a9b1fabc05
Anniymm <animaisuradze1999@gmail.com>
2024-09-23 23:29:13 +0400

    add urls and views - need to be reviewed

--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class ProjectView(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    # permission_classes = [IsAuthenticated]
+
+class TaskView(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskStatusView(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TimerView(viewsets.ModelViewSet):
+    queryset = Timer.objects.all()
+    serializer_class = TimerSerializer
+
