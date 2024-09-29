addd6abf6aaf907ecfde96c0d9a087410de15ccd
Anniymm <animaisuradze1999@gmail.com>
2024-09-29 18:32:10 +0400

    add corssettings

--- project/settings.py
+++ project/settings.py
@@ -31,12 +31,14 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'invitations',
     'app',
+    "corsheaders",
     'dashboard',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -44,6 +46,18 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173/",
+]
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
 ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
