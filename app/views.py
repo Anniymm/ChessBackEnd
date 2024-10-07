43924a81751bcfbd3a4ee37c14397324f5e233d7
Anniymm <animaisuradze1999@gmail.com>
2024-10-07 20:42:30 +0400

    modifications

---
+++ app/views.py
@@ -0,0 +1,46 @@
+# from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .serializers import *
+from .models import *
+from rest_framework.permissions import IsAuthenticated
+
+class ProjectView(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        
+        # manytomany fieldebistvis mwhirdeba rom mere iq carieli list ar mivigho
+        project = Project.objects.create(
+            name=data['name'],
+            description=data['description'],
+            start_date=data['start_date'],
+            end_date=data['end_date'],
+            created_by=request.user
+            
+        )
+
+        # project.customers.set(data.get('customers', []))
+        # project.contractors.set(data.get('contractors', []))
+        project.save()
+        serializer = self.get_serializer(project)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+class TaskView(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+class TaskStatusView(viewsets.ModelViewSet):
+    queryset = TaskStatus.objects.all()
+    serializer_class = TaskStatusSerializer
+    permission_classes = [IsAuthenticated]
+
+class TimerView(viewsets.ModelViewSet):
+    queryset = Timer.objects.all()
+    serializer_class = TimerSerializer
+    permission_classes = [IsAuthenticated]
+
